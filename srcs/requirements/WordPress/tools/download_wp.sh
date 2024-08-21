ea28f8c52fd018ab93311ac1888ac79a1ee341a6
nschutz <nschutz@student.42wolfsburg.de>
2024-08-21 14:30:00 +0200

    double worpress download now avoided

--- srcs/requirements/WordPress/tools/download_wp.sh
+++ srcs/requirements/WordPress/tools/download_wp.sh
@@ -5,22 +5,29 @@ WORDPRESS_URL="http://wordpress.org/latest.tar.gz"
 
 # Create the installation directory if it doesn't exist
 if [ -f ./wp-config.php ]; then
-    echo "worpress already installed"
-fi
+    echo "Platzhalter"
+else
 
 # Download WordPress
-echo "Downloading WordPress"
 curl -o wordpress.tar.gz -SL "$WORDPRESS_URL"
 
 # Extract WordPress
-echo "Extracting WordPress"
 tar -xzf wordpress.tar.gz
 mv wordpress/* .
 
 # Clean up the tar.gz file
-echo "Cleaning up..."
 rm wordpress.tar.gz
 rm -rf wordpress
 
 
-echo "WordPress downloaded and installed"
+sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
+sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
+sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
+sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php\
+
+cp wp-config-sample.php wp-config.php
+
+
+fi
+
+exec "$@"
